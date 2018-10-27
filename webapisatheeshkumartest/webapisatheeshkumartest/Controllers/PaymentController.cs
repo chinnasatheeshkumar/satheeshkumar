b73b8b664c56caecb697ae016b3a3dad6d6a0eae
chinnasatheeshkumar <chinnasatheeshkumar@gmail.com>
2018-10-27 10:20:43 +0530

    methods added

--- webapisatheeshkumartest/webapisatheeshkumartest/Controllers/PaymentController.cs
+++ webapisatheeshkumartest/webapisatheeshkumartest/Controllers/PaymentController.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using webapisatheeshkumartest.Models;
 
 namespace webapisatheeshkumartest.Controllers
 {
     public class PaymentController : ApiController
     {
+
+        [HttpPost]
+        public List<PaymentDetail> getPaymentDetails()
+        {
+            List<PaymentDetail> _dbPaymentDetailsList = new List<PaymentDetail>();
+
+            return _dbPaymentDetailsList;
+
+        }
+
+
     }
 }
---
+++ webapisatheeshkumartest/webapisatheeshkumartest/Models/PaymentDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace webapisatheeshkumartest.Models
+{
+    [DataContract]
+    public class PaymentDetail
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public long ApplicationId { get; set; }
+        [DataMember]
+        public string Type { get; set; }
+        [DataMember]
+        public string Summary { get; set; }
+        [DataMember]
+        public DateTime PostingDate { get; set; }
+        [DataMember]
+        public bool IsCleared { get; set; }
+        [DataMember]
+        public DateTime ClearedDate { get; set; }
+    }
+}
